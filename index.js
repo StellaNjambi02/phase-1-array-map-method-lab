6edcc8d6ffeb39e548cc19788053e15289c7a197
StellaNjambi02 <njambindungistella@gmail.com>
2024-07-02 13:18:38 +0300

    array map method

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      // Capitalize the first letter of the word while keeping the rest of the characters as they are
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
+};
+console.log(titleCased());
